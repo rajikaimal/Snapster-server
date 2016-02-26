996fb66c6bed61536d7f578a456eebaced1af162
rajikaimal <inbox.rajika@gmail.com>
2016-02-26 18:31:31 +0530

    express.js entry point

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/snap', function(req,res) {
+	res.send('Got it !');
+});
+
+app.listen(port, function() {
+	console.log('Server running on port %s', port);
+});
