b855802877f6dbef942c08ad523a3bfe233cab3d
rajikaimal <inbox.rajika@gmail.com>
2016-03-05 16:12:36 +0530

    jscs: code style check

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 80;
 
-app.get('/', function(req,res) {
-	console.log('log ...');
-	res.send('sample route');
+app.get('/', function(req, res) {
+  console.log('log ...');
+  res.send('sample route');
 });
 
-app.get('/snap', function(req,res) {
-	res.send('Got it !');
+app.get('/snap', function(req, res) {
+  res.send('Got it !');
 });
 
 app.listen(port, function() {
-	console.log('Server running on port %s', port);
+  console.log('Server running on port %s', port);
 });
