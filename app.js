94050e0900486982bd634fbf8604817a88a9360d
rajikaimal <inbox.rajika@gmail.com>
2016-03-11 21:01:01 +0530

    route for saving a post

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 80;
+var jsonfile = require('jsonfile');
+var appendjson = require('appendjson');
+var file = './snaps.json';
+var port = process.env.PORT || 3000;
+var fs = require('fs');
 
 app.get('/', function(req, res) {
   console.log('log ...');
@@ -9,6 +13,37 @@ app.get('/', function(req, res) {
 
 app.get('/snap', function(req, res) {
   res.send('Got it !');
+});
+
+app.get('/posts', function(req, res) {
+  jsonfile.readFile(file, function(err, obj) {
+    if (err) throw err;
+    var names = obj.posts.map(function(item) {
+      return item;
+    });
+
+    res.json(names);
+  });
+});
+
+app.post('/api/post', function(req, res) {
+  var post = {
+    name: 'Kendall Jenner',
+    datetime: '2016-03-19 08-08',
+    image: 'imagepath',
+    comments: {
+      username: 'rajika',
+      comment: 'sample comment',
+    },
+  };
+  var posts = fs.readFileSync('./snaps.json');
+  var config = JSON.parse(posts);
+
+  config.push(post);
+
+  var configJSON = JSON.stringify(config);
+
+  fs.writeFileSync('./snaps.json', configJSON);
 });
 
 app.listen(port, function() {
