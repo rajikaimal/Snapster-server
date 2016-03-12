6b58cbd65360930457cad9f4425d3e209fdac880
rajikaimal <inbox.rajika@gmail.com>
2016-03-12 15:49:48 +0530

    image upload

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 mongoose.connect('mongodb://rajika:miyoungrae123@ds011389.mlab.com:11389/heroku_mk054pc0');
 
@@ -17,19 +20,30 @@ app.get('/', function(req, res) {
   res.json({env: 'testing'});
 });
 
-//post to funny feed 
-app.post('/api/post/funnyfeed', function(req, res) {
+//post to funny feed
+app.post('/api/post/funnyfeed', multipartMiddleware, function(req, res) {
   var username = req.body.username;
   var time = new Date();
-  var image = req.body.image;
+  var image = req.files.image;
+
+  var tmpPath = image.path;
+  var targetPath = 'feeds/funny/' + username + '_' + time + image.name;
+
+  fs.rename(tmpPath, targetPath, function(err) {
+    if (err) throw err;
+
+    fs.unlink(tmpPath, function() {
+      if (err) throw err;
+    });
+  });
 
   var post = {
     username: username,
     datetime: time,
-    image: image,
+    image: targetPath,
     likes: 0,
   };
-
+  
   var newPost = new Post(post);
   newPost.save(function(err) {
     if (err) {
@@ -47,7 +61,6 @@ app.post('/api/post/funnyfeed', function(req, res) {
     }
   });
 });
-
 
 //retrieves post based on id (mongodb _id)
 app.get('/api/post/funnyfeed/:id', function(req, res) {
