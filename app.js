a092ed6ab69848d2d1d12fee304d89db7952f6f8
rajikaimal <inbox.rajika@gmail.com>
2016-04-09 01:04:59 +0530

    add response object for realtime feed

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var cloudinary = require('cloudinary');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 cloudinary.config({
   cloud_name: 'rajikaimal',
@@ -29,6 +31,7 @@ app.get('/', function(req, res) {
 
 //post to funny feed
 app.post('/api/post/funnyfeed', multipartMiddleware, function(req, res) {
+  
   var username = req.body.username;
   var time = new Date();
   var image = req.files.image;
@@ -56,16 +59,20 @@ app.post('/api/post/funnyfeed', multipartMiddleware, function(req, res) {
             console.log(item._id + ' ' + item.username);
           });
         });
-
+        io.emit('newfunnypost', post);
         res.json({done: true});
       }
     });
   });
 
+  
+
+
 });
 
 //retrieves post based on id (mongodb _id)
 app.get('/api/post/funnyfeed/:id', function(req, res) {
+  console.log('Funny feed');
   var id = req.params.id;
 
   //search query
@@ -77,6 +84,7 @@ app.get('/api/post/funnyfeed/:id', function(req, res) {
 });
 
 app.get('/api/feed/funny', function(req, res) {
+  console.log('Funny feed');
   Post.find(function(err, posts) {
     if (err) console.log(err);
 
@@ -84,6 +92,6 @@ app.get('/api/feed/funny', function(req, res) {
   });
 });
 
-app.listen(port, function() {
+server.listen(port, function() {
   console.log('Server running on port %s', port);
 });
