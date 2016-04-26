944da4c3e87ad5316f60c595f46bc96982027262
rajikaimal <inbox.rajika@gmail.com>
2016-04-26 13:35:26 +0530

    move routes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var fs = require('fs');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var mongoConfig = require('./config/mongo');
@@ -13,8 +12,13 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var router = express.Router();
 var commentRoutes = require('./routes/comments');
+var postRoutes = require('./routes/posts');
 
 app.use(router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 cloudinary.config({
   cloud_name: 'rajikaimal',
@@ -28,110 +32,10 @@ var Post = mongoose.model('Post', { username: String, datetime: String, image: S
 var Comment = mongoose.model('Comment', { postid: String, username: String, datetime: String, comment: String});
 
 commentRoutes(router, Comment);
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+postRoutes(router,multipartMiddleware,cloudinary,io,Post);
 
 app.get('/', function(req, res) {
   res.json({env: 'testing'});
-});
-
-//post to funny feed
-app.post('/api/post/funnyfeed', multipartMiddleware, function(req, res) {
-  
-  var username = req.body.username;
-  var time = new Date();
-  var image = req.files.image;
-
-  var tmpPath = image.path;
-
-  cloudinary.uploader.upload(tmpPath, function(result) {
-    console.log(result);
-    var post = {
-      username: username,
-      datetime: time,
-      image: 'v' + result.version + '/' + result.public_id,
-      likes: 0,
-    };
-
-    var newPost = new Post(post);
-    newPost.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        Post.find(function(err, onePost) {
-          if (err) console.log(err);
-
-          onePost.map(function(item) {
-            console.log(item._id + ' ' + item.username);
-          });
-        });
-        io.emit('newfunnypost', post);
-        res.json({done: true});
-      }
-    });
-  });
-
-  
-
-
-});
-
-//retrieves post based on id (mongodb _id)
-app.get('/api/post/funnyfeed/:id', function(req, res) {
-  console.log('Funny feed');
-  var id = req.params.id;
-
-  //search query
-
-  Post.find({_id: id}, function(err, userpost) {
-    res.json({ post: userpost[0] });
-  });
-
-});
-
-app.post('/api/post/funnyfeed/comment', function(req, res) {
-  var postid = req.body.postid;
-  var username = req.body.username;
-  var time = new Date();
-  var comment = req.body.comment;
-
-  var comment = {
-      postid: postid,
-      username: username,
-      datetime: time,
-      comment: comment
-  };
-
-  console.log(comment);
-
-  var newComment = new Comment(comment);
-  newComment.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      Comment.find(function(err, onePost) {
-        if (err) console.log(err);
-
-        onePost.map(function(item) {
-          console.log(item._id + ' ' + item.username);
-        });
-      });
-      res.json({done: true});
-    }
-  });
-});
-
-
-app.get('/api/feed/funny', function(req, res) {
-  Post.find(function(err, posts) {
-    if (err) console.log(err);
-
-    res.json(posts);
-  });
 });
 
 server.listen(port, function() {
