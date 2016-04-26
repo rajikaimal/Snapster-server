7fb3762476c97d3f324272f49f4a24edf29bffe7
rajikaimal <inbox.rajika@gmail.com>
2016-04-26 17:11:19 +0530

    change likes routes

---
+++ routes/likes.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var likesRouter = function (router, multipartMiddleware, io, Like) {
+  router.post('/api/post/like', multipartMiddleware, function (req, res) {
+
+    var postId = req.body.postid;
+    var username = req.body.username;
+    var time = new Date();
+
+    var like = {
+      postid: postId,
+      username: username,
+      datetime: time,
+    };
+    console.log(like);
+
+    var newLike = new Like(like);
+
+    newLike.save(function (err, docs) {
+      if (err) console.log(err);
+
+      console.log(docs);
+      res.json({
+        status: 'done',
+      });
+    });
+
+  })
+
+  .get('/api/post/likes', function (req, res) {
+    var postId = req.query.postid;
+    console.log(postId);
+    Like.find({'postid': postId}, function (err, docs) {
+      if (err)
+
+      console.log(docs);
+      res.json(docs);
+    });
+  });
+
+};
+
+module.exports = likesRouter;
