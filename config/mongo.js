d388d4ffcea29365b99d25a2c58f7a35e6138434
rajikaimal <inbox.rajika@gmail.com>
2016-04-26 13:49:21 +0530

    jscs: format

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,5 @@
 var credentials = {
-	user: 'mongodb://rajikaimal:sicasica123@ds021681.mlab.com:21681/snapster'
+  user: 'mongodb://rajikaimal:sicasica123@ds021681.mlab.com:21681/snapster',
 };
 
 module.exports = credentials;
--- routes/comments.js
+++ routes/comments.js
@@ -1,46 +1,47 @@
 var express = require('express');
 
-var commentRouter = function(router,Comment) {
-	router.get('/api/feed/funny/comments/:id', function(req, res) {
-	  var postId = req.params.id;
-	  Comment.find({postid: postId}, function(err, posts) {
-	    if (err) console.log(err);
+var commentRouter = function (router, Comment) {
+  router.get('/api/feed/funny/comments/:id', function (req, res) {
+    var postId = req.params.id;
+    Comment.find({ postid: postId }, function (err, posts) {
+      if (err) console.log(err);
 
-	    res.json(posts);
-	  });
-	})
+      res.json(posts);
+    });
+  })
 
-	.post('/api/post/funnyfeed/comment', function(req, res) {
-	  var postid = req.body.postid;
-	  var username = req.body.username;
-	  var time = new Date();
-	  var comment = req.body.comment;
+  .post('/api/post/funnyfeed/comment', function (req, res) {
+    var postid = req.body.postid;
+    var username = req.body.username;
+    var time = new Date();
+    var comment = req.body.comment;
 
-	  var comment = {
-	      postid: postid,
-	      username: username,
-	      datetime: time,
-	      comment: comment
-	  };
+    var comment = {
+        postid: postid,
+        username: username,
+        datetime: time,
+        comment: comment,
+      };
 
-	  console.log(comment);
+    console.log(comment);
 
-	  var newComment = new Comment(comment);
-	  newComment.save(function(err) {
-	    if (err) {
-	      console.log(err);
-	    } else {
-	      Comment.find(function(err, onePost) {
-	        if (err) console.log(err);
+    var newComment = new Comment(comment);
+    newComment.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        Comment.find(function (err, onePost) {
+          if (err) console.log(err);
 
-	        onePost.map(function(item) {
-	          console.log(item._id + ' ' + item.username);
-	        });
-	      });
-	      res.json({done: true});
-	    }
-	  });
-	});
+          onePost.map(function (item) {
+            console.log(item._id + ' ' + item.username);
+          });
+        });
+
+        res.json({ done: true });
+      }
+    });
+  });
 };
 
 module.exports = commentRouter;
