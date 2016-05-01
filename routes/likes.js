51763d55f943a6bb7993348af5178cbab9e589cb
rajikaimal <inbox.rajika@gmail.com>
2016-05-01 19:31:46 +0530

    log: test

--- routes/likes.js
+++ routes/likes.js
@@ -3,7 +3,7 @@ var express = require('express');
 var likesRouter = function (router, multipartMiddleware, io, Like) {
   router.post('/api/post/like', multipartMiddleware, function (req, res) {
 
-    console.log(req.params);
+    console.log(req);
 
     var postId = req.body.postid;
     var username = req.body.username;
