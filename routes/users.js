f0e4a9b25561676b14eba5f206aa9120d8e20324
rajikaimal <inbox.rajika@gmail.com>
2016-05-06 00:55:25 +0530

    login

--- routes/users.js
+++ routes/users.js
@@ -33,9 +33,9 @@ var usersRouter = function (router, User, multipartMiddleware) {
 
   .get('/api/user/login', function (req, res) {
     var username = req.query.username;
-    var email = req.query.email;
+    var password = req.query.password;
 
-    User.find({ postid: postId }, function (err, docs) {
+    User.find({ username: username, password: password }, function (err, docs) {
       if (err)
 
       if (docs != null) {
